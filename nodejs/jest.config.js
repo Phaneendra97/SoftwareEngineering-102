df4fa8ae916b34b26fb67ee832771f27e042e9a9
phaneendra97 <phaneendra1997@gmail.com>
2023-05-22 17:17:29 -0700

    added coverage to testing

---
+++ nodejs/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: ['controllers/*.js'], // Adjust the glob pattern to match your source code files
+    coverageDirectory: 'coverage',
+  };
