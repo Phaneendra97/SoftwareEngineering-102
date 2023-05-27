64135d0a6f7e7b556eec8accf5ca8249d042d793
Srikar <saisrikar@sais-MacBook-Pro.local>
2023-05-26 20:40:39 -0700

    fixing health url's # success Signed-off-by: Srikar <saisrikar@sais-MacBook-Pro.local>

--- nodejs/controllers/healthController.js
+++ nodejs/controllers/healthController.js
@@ -1,18 +1,18 @@
 var mongoose = require("mongoose"),
-  jwt = require("jsonwebtoken"),
-  bcrypt = require("bcrypt");
+    jwt = require("jsonwebtoken"),
+    bcrypt = require("bcrypt");
 deptList = mongoose.model("dept_list");
 const winston = require("winston");
 
 const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: "logfile.log" }),
-  ],
+    level: "info",
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: "logfile.log" }),
+    ],
 });
 
 exports.healthCheck = function (req, res) {
-  return res.send("success");
+    return res.send("# success");
 };
